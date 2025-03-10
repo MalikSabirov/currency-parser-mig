6c092cb8ad66ddb83d088e2275d8fe935c22d04f
MalikSabirov <maaliksabirov@gmail.com>
2025-03-10 16:06:39 +0500

    feat(models): add currency model

--- pkg/models/currency.go
+++ pkg/models/currency.go
@@ -1,1 +1,11 @@
 package models
+
+import "time"
+
+type Currency struct {
+	ID           int       `json:"id,omitempty"`
+	CurrencyCode string    `json:"currency_code"`
+	BuyRate      float64   `json:"buy_rate"`
+	SellRate     float64   `json:"sell_rate"`
+	Timestamp    time.Time `json:"timestamp"`
+}
