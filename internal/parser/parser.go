43e8bc6e25f5ad844659b7652aff2f4b2735d769
MalikSabirov <maaliksabirov@gmail.com>
2025-03-10 16:08:32 +0500

    feat(parser): add time parsing and collect currency object

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,10 +1,14 @@
 package parser
 
 import (
+	"currency-parser-mig/pkg/models"
+	"currency-parser-mig/pkg/utils"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -13,14 +17,14 @@ func ParseCurrencies() {
 	log.Println("Start parsing...")
 
 	migUrl := os.Getenv("MIG_BASE_URL")
-	if migUrl=="" {
+	if migUrl == "" {
 		log.Printf("Error loading MIG_BASE_URL from .env file")
 		return
 	}
 
 	resp, err := http.Get(migUrl)
 	if err != nil {
-		log.Printf("Failed to fetch webpage: %v", err)
+		log.Printf("Failed to get site: %v", err)
 		return
 	}
 	defer resp.Body.Close()
@@ -38,7 +42,44 @@ func ParseCurrencies() {
 
 	defer resp.Body.Close()
 
-	doc.Find(".informer table tr").Each(func(i int, s *goquery.Selection) {
+	currencies := make([]models.Currency, 0)
+
+	currencyBlock := doc.Find(".informer")
+
+	// date parse
+	var timestamp time.Time
+	dateBlock := currencyBlock.Find("h4").First()
+	dateStr := strings.TrimSpace(dateBlock.Text())
+
+	preposition := "на"
+
+	if !strings.Contains(dateStr, preposition) {
+		log.Printf("Failed to parse dateStr, there is no preposition \"%s\": %s", preposition, dateStr)
+		return
+	}
+
+	dateStr = strings.SplitN(dateStr, preposition, 2)[1] // get date without preposition
+	dateStr = strings.TrimSpace(dateStr)
+	dateStr, err = utils.ReplaceRussianMonth(dateStr)
+
+	if err != nil {
+		log.Printf("Error parsing date: %v", err)
+	}
+
+	// get UTC+5
+	loc, err := time.LoadLocation("Asia/Almaty")
+	if err != nil {
+		log.Printf("Error loading timezone:  %v", err)
+	}
+
+	if parsedTime, err := time.ParseInLocation(utils.ETALON_DATE, dateStr, loc); err == nil {
+		timestamp = parsedTime
+	} else {
+		log.Printf("Error parsing date: %v", err)
+	}
+
+	// currency code, buy, sell rate parse
+	currencyBlock.Find("table tr").Each(func(i int, s *goquery.Selection) {
 		currencyCode := s.Find("td.currency").Text()
 		if currencyCode == "" {
 			return
@@ -59,7 +100,13 @@ func ParseCurrencies() {
 			return
 		}
 
-		log.Println("buyRate value:", buyRate)
-		log.Println("sellRate value:", sellRate)
+		currencies = append(currencies, models.Currency{
+			CurrencyCode: strings.TrimSpace(currencyCode),
+			BuyRate:      buyRate,
+			SellRate:     sellRate,
+			Timestamp:    timestamp,
+		})
 	})
+
+	log.Print(currencies)
 }
